45706240c8a5a62eb9f3aee90617d6e42320ad1a
KaFaiC <choikafai2004@gmail.com>
2014-08-08 22:21:32 +0800

    Add form validation and loading spinner

--- public/app/js/controllers/playerController.js
+++ public/app/js/controllers/playerController.js
@@ -2,12 +2,15 @@ angular.module('NbaScraper')
   .controller('PlayerController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http){
     $scope.playerName = "";
     $scope.searchPlayer = function(playerName){
+      $scope.isProcessing = true;
       $http.post('/search', playerName)
         .success(function(data, status){
           $scope.playerStat = data;
+          $scope.isProcessing = false;
         })
         .error(function(data, status){
           $scope.playerStat = data;
+          $scope.isProcessing = false;
         });      
     }
   }])
