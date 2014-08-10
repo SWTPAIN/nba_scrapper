df1bc1962fc805860b4dbe664770c0c359b72828
KaFaiC <choikafai2004@gmail.com>
2014-08-10 18:19:41 +0800

    Modify the web scrapping helper and improve angular search input validation

---
+++ helpers/scrapeHelper.rb
@@ -0,0 +1,110 @@
+helpers do
+
+  def scrapping_player_stat(player_name)
+    #get the player name keyword for getting the correct url
+    firstname, lastname = player_name.strip.downcase!.split(" ")
+    player_keyword = lastname[0,5] + firstname[0,2]
+    player_info = {}
+
+    #findout which years the player play at least one game
+    url = "http://www.basketball-reference.com/players/#{lastname[0]}/#{player_keyword}01.html"
+
+    data = Nokogiri::HTML(open(url))
+    years_played = []
+
+    data.xpath("//table[@id='totals']/tbody/tr[not(contains(@class, 'italic_text'))]").each do |tr|
+      years_played << tr.css('td')[0].text[0,4].to_i + 1  
+    end
+
+    if (player_info[:fullName] = data.xpath("//h1").text()) != "File Not Found"
+      data.xpath("//table[@id='totals']/tbody/tr[not(contains(@class, 'italic_text'))]").each do |tr|
+        years_played << tr.css('td')[0].text[0,4].to_i + 1  
+      end
+      #There are two html structure for old player and new player.
+      if (data.xpath("//div[@class='person_image_offset']").empty?)
+        player_info[:position] = data.xpath("//div[@id='info_box']/p[2]/text()[1]").text()
+        player_info[:height] = data.xpath("//div[@id='info_box']/p[2]/text()[3]").text()
+        player_info[:weight] = data.xpath("//div[@id='info_box']/p[2]/text()[4]").text()
+      else
+        player_info[:position] = data.xpath("//div[@class='person_image_offset']/p[2]/text()[1]").text()
+        player_info[:height] = data.xpath("//div[@class='person_image_offset']/p[2]/text()[3]").text()
+        player_info[:weight] = data.xpath("//div[@class='person_image_offset']/p[2]/text()[4]").text()
+      end
+      player_info  = normalize_player_info(player_info)
+
+      if years_played.empty?
+        puts "The game has no game record."
+      else
+        yearly_game_stat = {yearlyGameStat: []}
+        years_played.each do |year|
+          url = "http://www.basketball-reference.com/players/#{lastname[0]}/#{player_keyword}01/gamelog/#{year}/"
+          data = Nokogiri::HTML(open(url))
+          year_game_stat = {year: year, gameStat: []}
+          data.xpath("//table[@id=\"pgl_basic\"]/tbody/tr[not(contains(@class, 'thead')) and not(contains(@class, 'italic_text')) and not(contains(@id, '.0'))]").each do |tr|
+            d = tr.css('td')
+            each_game = {
+              date: d[2].text.strip,
+              age: parse_age(d[3].text.strip),
+              team: d[4].text.strip,
+              homecourt_flag: d[5].text.strip.empty? ? 1 : 0,
+              opp: d[6].text.strip,
+              result: d[7].text.strip,
+              gs_flag: d[8].text.strip,
+              mp: d[9].text.strip,
+              fg: d[10].text.strip,
+              fga: d[11].text.strip,
+              three_pt: d[13].text.strip,
+              three_pta: d[14].text.strip,
+              ft: d[16].text.strip,
+              fta: d[17].text.strip,
+              orb: d[19].text.strip,
+              drb: d[20].text.strip,
+              stl: d[22].text.strip,
+              blk: d[23].text.strip,
+              tov: d[24].text.strip,
+              pf: d[25].text.strip,
+              pts: d[26].text.strip,
+              plus_minus: d[28].text.strip
+            }
+            year_game_stat[:gameStat] << each_game
+          end
+          yearly_game_stat[:yearlyGameStat] << year_game_stat
+        end
+      end
+    else
+      puts "There is no such player"
+    end  
+    player_info.merge(yearly_game_stat)
+  end
+
+  private
+
+  def normalize_player_info(player_info)
+    player_info[:position] = normalize_position(player_info[:position])
+    player_info[:height] = inches_to_cm(player_info[:height])
+    player_info[:weight] = parse_weight(player_info[:weight])
+    player_info
+  end
+
+  def normalize_position(position_string)
+    position_string.split("and").map do |pos| 
+      pos = pos.strip.gsub(/[^\w\s]/, '').split(" ").inject("") do |short_pos, word|
+        short_pos += word[0]
+      end
+    end
+  end
+
+  def inches_to_cm(length)
+    feet, inches = length.split("-")
+    (feet.to_i * 30.48 + inches.to_i * 2.54).round
+  end
+
+  def parse_weight(weight)
+    weight.split(' ')[0].to_i
+  end
+
+  def parse_age(age)
+    age.sub('-','.').to_f
+  end
+
+end
--- public/app/js/controllers/playerController.js
+++ public/app/js/controllers/playerController.js
@@ -13,44 +13,87 @@ angular.module('NbaScraper')
           $scope.isProcessing = false;
         });      
     };
-    $scope.data = [{
-        key: "Cumulative Return",
-        values: [
-            { "label" : "A" , "value" : -29.765957771107 },
-            { "label" : "B" , "value" : 0 },
-            { "label" : "C" , "value" : 32.807804682612 },
-            { "label" : "D" , "value" : 196.45946739256 },
-            { "label" : "E" , "value" : 0.19434030906893 },
-            { "label" : "F" , "value" : -98.079782601442 },
-            { "label" : "G" , "value" : -13.925743130903 },
-            { "label" : "H" , "value" : -5.1387322875705 }
-        ]
-    }];
-    $scope.options = {
-        chart: {
-            type: 'discreteBarChart',
-            height: 450,
-            margin : {
-                top: 20,
-                right: 20,
-                bottom: 60,
-                left: 55
+
+  $scope.options = {
+            chart: {
+                type: 'lineChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 40,
+                    left: 55
+                },
+                x: function(d){ return d.x; },
+                y: function(d){ return d.y; },
+                useInteractiveGuideline: true,
+                dispatch: {
+                    stateChange: function(e){ console.log("stateChange"); },
+                    changeState: function(e){ console.log("changeState"); },
+                    tooltipShow: function(e){ console.log("tooltipShow"); },
+                    tooltipHide: function(e){ console.log("tooltipHide"); }
+                },
+                xAxis: {
+                    axisLabel: 'Time (ms)'
+                },
+                yAxis: {
+                    axisLabel: 'Voltage (v)',
+                    tickFormat: function(d){
+                        return d3.format('.02f')(d);
+                    },
+                    axisLabelDistance: 30
+                },
+                callback: function(chart){
+                    console.log("!!! lineChart callback !!!");
+                }
             },
-            x: function(d){ return d.label; },
-            y: function(d){ return d.value; },
-            showValues: true,
-            valueFormat: function(d){
-                return d3.format(',.4f')(d);
+            title: {
+                enable: true,
+                text: 'Title for Line Chart'
             },
-            transitionDuration: 500,
-            xAxis: {
-                axisLabel: 'X Axis'
+            subtitle: {
+                enable: true,
+                text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
+                css: {
+                    'text-align': 'center',
+                    'margin': '10px 13px 0px 7px'
+                }
             },
-            yAxis: {
-                axisLabel: 'Y Axis',
-                axisLabelDistance: 30
+            caption: {
+                enable: true,
+                html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
+                css: {
+                    'text-align': 'justify',
+                    'margin': '10px 13px 0px 7px'
+                }
             }
-        }
-    };
-        
+        };
+
+        // $scope.data = sinAndCos();
+        $scope.data = [
+          {values: [{x:1, y:2},{x:2, y:3}, {x:3, y:11}]}
+        ];
+
+        /*Random Data Generator */
+        function sinAndCos() {
+            var sin = [],sin2 = [],
+                cos = [];
+
+            //Data is represented as an array of {x,y} pairs.
+            for (var i = 0; i < 100; i++) {
+                sin.push({x: i, y: Math.sin(i/10)});
+                sin2.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) *0.25 + 0.5});
+                cos.push({x: i, y: .5 * Math.cos(i/10+ 2) + Math.random() / 10});
+            }
+
+            //Line chart data should be sent as an array of series objects.
+            return [
+                {
+                    values: sin,      //values - represents the array of {x,y} data points
+                    key: 'Sine Wave', //key  - the name of the series.
+                    color: '#ff7f0e'  //color - optional: choose your own line color.
+                }
+            ];
+        };
+
   }])
